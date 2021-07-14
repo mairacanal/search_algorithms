88e41c48d531cc010ad17856ad6908b01526569c
mairacanal <maira.canal@usp.br>
2021-07-14 17:25:40 -0300

    Adding Template files

---
+++ Templates/exercicio2b.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
+// Definição das variaveis que controlam a medição de tempo
+clock_t _ini, _fim;
+
+// Definição do tipo booleano
+typedef unsigned char bool;
+#define TRUE  1
+#define FALSE 0
+
+// Definição do tipo string
+typedef char * string;
+
+#define MAX_STRING_LEN 20
+
+
+unsigned converter(string s) {
+   unsigned h = 0;
+   for (int i = 0; s[i] != '\0'; i++) 
+      h = h * 256 + s[i];
+   return h;
+}
+
+string* ler_strings(const char * arquivo, const int n)
+{
+    FILE* f = fopen(arquivo, "r");
+    
+    string* strings = (string *) malloc(sizeof(string) * n);
+
+    for (int i = 0; !feof(f); i++) {
+        strings[i] = (string) malloc(sizeof(char) * MAX_STRING_LEN);
+        fscanf(f, "%s\n", strings[i]);
+    }
+
+    fclose(f);
+
+    return strings;
+}
+
+void inicia_tempo()
+{
+    srand(time(NULL));
+    _ini = clock();
+}
+
+double finaliza_tempo()
+{
+    _fim = clock();
+    return ((double) (_fim - _ini)) / CLOCKS_PER_SEC;
+}
+
+
+unsigned h_div(unsigned x, unsigned i, unsigned B)
+{
+    return ((x % B) + i) % B;
+}
+
+unsigned h_mul(unsigned x, unsigned i, unsigned B)
+{
+    const double A = 0.6180;
+    return  ((int) ((fmod(x * A, 1) * B) + i)) % B;
+}
+
+int main(int argc, char const *argv[])
+{
+    const int N = 50000;
+    const int M = 70000;
+    const int B = 150001;
+
+    unsigned colisoes = 0;
+    unsigned encontrados = 0;
+
+    string* insercoes = ler_strings("strings_entrada.txt", N);
+    string* consultas = ler_strings("strings_busca.txt", M);
+
+
+    // cria tabela hash com hash por hash duplo
+
+    // inserção dos dados na tabela hash
+    inicia_tempo();
+    for (int i = 0; i < N; i++) {
+        // inserir insercoes[i] na tabela hash
+    }
+    double tempo_insercao = finaliza_tempo();
+
+    // busca dos dados na tabela hash
+    inicia_tempo();
+    for (int i = 0; i < M; i++) {
+        // buscar consultas[i] na tabela hash
+    }
+    double tempo_busca = finaliza_tempo();
+
+
+    printf("Colisões na inserção: %d\n", colisoes);
+    printf("Tempo de inserção   : %fs\n", tempo_insercao);
+    printf("Tempo de busca      : %fs\n", tempo_busca);
+    printf("Itens encontrados   : %d\n", encontrados);
+
+    return 0;
+}
---
+++ Templates/exercicio2c.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+
+// Definição das variaveis que controlam a medição de tempo
+clock_t _ini, _fim;
+
+// Definição do tipo booleano
+typedef unsigned char bool;
+#define TRUE  1
+#define FALSE 0
+
+// Definição do tipo string
+typedef char * string;
+
+#define MAX_STRING_LEN 20
+
+
+unsigned converter(string s) {
+   unsigned h = 0;
+   for (int i = 0; s[i] != '\0'; i++) 
+      h = h * 256 + s[i];
+   return h;
+}
+
+string* ler_strings(const char * arquivo, const int n)
+{
+    FILE* f = fopen(arquivo, "r");
+    
+    string* strings = (string *) malloc(sizeof(string) * n);
+
+    for (int i = 0; !feof(f); i++) {
+        strings[i] = (string) malloc(sizeof(char) * MAX_STRING_LEN);
+        fscanf(f, "%s\n", strings[i]);
+    }
+
+    fclose(f);
+
+    return strings;
+}
+
+void inicia_tempo()
+{
+    srand(time(NULL));
+    _ini = clock();
+}
+
+double finaliza_tempo()
+{
+    _fim = clock();
+    return ((double) (_fim - _ini)) / CLOCKS_PER_SEC;
+}
+
+unsigned h_div(unsigned x, unsigned B)
+{
+    return x % B;
+}
+
+unsigned h_mul(unsigned x, unsigned B)
+{
+    const double A = 0.6180;
+    return fmod(x * A, 1) * B;
+}
+
+int main(int argc, char const *argv[])
+{
+    const int N = 50000;
+    const int M = 70000;
+    const int B = 150001;
+
+    unsigned colisoes_h_div = 0;
+    unsigned colisoes_h_mul = 0;
+
+    unsigned encontrados_h_div = 0;
+    unsigned encontrados_h_mul = 0;
+
+    string* insercoes = ler_strings("strings_entrada.txt", N);
+    string* consultas = ler_strings("strings_busca.txt", M);
+    
+
+    // cria tabela hash com hash por divisão
+    
+    // inserção dos dados na tabela hash com hash por divisão
+    inicia_tempo();
+    for (int i = 0; i < N; i++) {
+        // inserir insercoes[i] na tabela hash
+    }
+    double tempo_insercao_h_div = finaliza_tempo();
+
+    // busca dos dados na tabela hash com hash por divisão
+    inicia_tempo();
+    for (int i = 0; i < M; i++) {
+        // buscar consultas[i] na tabela hash
+    }
+    double tempo_busca_h_div = finaliza_tempo();
+
+    // destroi tabela hash com hash por divisão
+
+
+
+
+    // cria tabela hash com hash por multiplicação
+
+    // inserção dos dados na tabela hash com hash por multiplicação
+    inicia_tempo();
+    for (int i = 0; i < N; i++) {
+        // inserir insercoes[i] na tabela hash
+    }
+    double tempo_insercao_h_mul = finaliza_tempo();
+
+    // busca dos dados na tabela hash com hash por multiplicação
+    inicia_tempo();
+    for (int i = 0; i < M; i++) {
+        // buscar consultas[i] na tabela hash
+    }
+    double tempo_busca_h_mul = finaliza_tempo();
+
+    // destroi tabela hash com hash por multiplicação
+
+
+
+    printf("Hash por Divisão\n");
+    printf("Colisões na inserção: %d\n", colisoes_h_div);
+    printf("Tempo de inserção   : %fs\n", tempo_insercao_h_div);
+    printf("Tempo de busca      : %fs\n", tempo_busca_h_div);
+    printf("Itens encontrados   : %d\n", encontrados_h_div);
+    printf("\n");
+    printf("Hash por Multiplicação\n");
+    printf("Colisões na inserção: %d\n", colisoes_h_mul);
+    printf("Tempo de inserção   : %fs\n", tempo_insercao_h_mul);
+    printf("Tempo de busca      : %fs\n", tempo_busca_h_mul);
+    printf("Itens encontrados   : %d\n", encontrados_h_mul);
+
+    return 0;
+}
