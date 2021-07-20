486d3bd6b86341b923fd907cc3f780d41264029e
mairacanal <maira.canal@usp.br>
2021-07-20 17:37:43 -0300

    Adding Vetor struct

--- exercicio1d.c
+++ exercicio1d.c
@@ -17,6 +17,11 @@ typedef struct {
     int pos;
 } TabelaIndice;
 
+typedef struct {
+    int* vetor;
+    int tamanho;
+} Vetor;
+
 // ========================================================================================================
 
 int* ler_inteiros(const char * arquivo, const int n)
@@ -47,26 +52,26 @@ double finaliza_tempo()
 
 // DESENVOLVIDA PELO GRUPO ================================================================================
 
-int maximo_elemento(int* vetor, unsigned tamanho) 
+int maximo_elemento(Vetor *tabela) 
 {
     int max = 0;
 
-    for (int i = 0; i < tamanho; i++) 
-        if(vetor[i] > max) max = vetor[i];
+    for (int i = 0; i < tabela->tamanho; i++) 
+        if(tabela->vetor[i] > max) max = tabela->vetor[i];
 
     return max;
 }
 
-void radix_counting_sort(int* vetor, unsigned tamanho, int posicao) 
+void radix_counting_sort(Vetor *tabela, int posicao) 
 {
     int k = 10;
     long *contagem = (long *) calloc(k, sizeof(long));
-    long *final = (long *) calloc(tamanho, sizeof(long));
+    long *final = (long *) calloc(tabela->tamanho, sizeof(long));
     long chave;
 
-    for(int i = 0; i < tamanho; i++) 
+    for(int i = 0; i < tabela->tamanho; i++) 
     {
-        chave = vetor[i]/posicao;
+        chave = tabela->vetor[i]/posicao;
         chave = chave % k;
         contagem[chave]++;
     }
@@ -74,44 +79,44 @@ void radix_counting_sort(int* vetor, unsigned tamanho, int posicao)
     for(int i = 1; i < k; i++)
         contagem[i] += contagem[i - 1];
 
-    for(int i = (tamanho - 1); i >= 0; i--) 
+    for(int i = (tabela->tamanho - 1); i >= 0; i--) 
     {
-        chave = vetor[i]/posicao;
+        chave = tabela->vetor[i]/posicao;
         chave = chave % k;
         contagem[chave]--;
-        final[contagem[chave]] = vetor[i];
+        final[contagem[chave]] = tabela->vetor[i];
     }
 
-    for(int i = 0; i < tamanho; i++)
-        vetor[i] = final[i];
+    for(int i = 0; i < tabela->tamanho; i++)
+        tabela->vetor[i] = final[i];
 
     free(contagem);
     free(final);
 }
 
-void radix_sort(int* vetor, unsigned tamanho) 
+void radix_sort(Vetor *tabela) 
 {
-    int max = maximo_elemento(vetor, tamanho);
+    int max = maximo_elemento(tabela);
 
     for (int posicao = 1; max/posicao > 0; posicao *= 10)
-        radix_counting_sort(vetor, tamanho, posicao);
+        radix_counting_sort(tabela, posicao);
 }
 
-TabelaIndice* criar_TabelaIndice(int* tabela, int N, int T) 
+TabelaIndice* criar_TabelaIndice(Vetor* tabela, int T) 
 {
     TabelaIndice* tabelaIndice;
 
     tabelaIndice = (TabelaIndice *) malloc(T * sizeof(TabelaIndice));
 
     for (int i = 0; i < T; i++) {
-        tabelaIndice[i].pos = i * (N / T);
-        tabelaIndice[i].kindex = tabela[tabelaIndice[i].pos];
+        tabelaIndice[i].pos = i * (tabela->tamanho / T);
+        tabelaIndice[i].kindex = tabela->vetor[tabelaIndice[i].pos];
     }
 
     return tabelaIndice;
 }
 
-int busca_sequencial_indexada(int* tabela, TabelaIndice* tabelaIndice, int elemento, int tamanho, int T) 
+int busca_sequencial_indexada(Vetor* tabela, TabelaIndice* tabelaIndice, int elemento, int T) 
 {
     int i;
 
@@ -121,8 +126,8 @@ int busca_sequencial_indexada(int* tabela, TabelaIndice* tabelaIndice, int eleme
         return (elemento == tabelaIndice[i].kindex) ? i : -1;
 
     // Busca sequencial no vetor de elemento a partir da posicao indexada
-    for (i = tabelaIndice[i - 1].pos; i < tamanho; i++)
-        if (elemento == tabela[i]) return i;
+    for (i = tabelaIndice[i - 1].pos; i < tabela->tamanho; i++)
+        if (elemento == tabela->vetor[i]) return i;
 
     return -1;
 
@@ -141,18 +146,23 @@ int main(int argc, char const *argv[])
     int* consultas = ler_inteiros("inteiros_busca.txt", N);
     
     TabelaIndice* tabelaIndice;
+    Vetor tabela;
+
+    // Inicializando tabela de busca
+    tabela.tamanho = N;
+    tabela.vetor = entradas;
 
     // ordenar entrada
-    radix_sort(entradas, N);
+    radix_sort(&tabela);
 
     // criar tabela de indice
-    tabelaIndice = criar_TabelaIndice(entradas, N, T);
+    tabelaIndice = criar_TabelaIndice(&tabela, T);
 
     // realizar consultas na tabela de indices 
     inicia_tempo();
     for (int i = 0; i < N; i++) {
         // buscar o elemento consultas[i] na entrada
-        if (busca_sequencial_indexada(entradas, tabelaIndice, consultas[i], N, T) != -1) 
+        if (busca_sequencial_indexada(&tabela, tabelaIndice, consultas[i], T) != -1) 
             encontrados++;
     }
     double tempo_busca = finaliza_tempo();
